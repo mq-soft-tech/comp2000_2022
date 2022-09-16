dcfcf8a55c9cc15ada920a011b81c88e4a17737d
Damian Jurd <damian.jurd@mq.edu.au>
2022-09-16 22:36:04 +1000

    solution to task 20

--- src/Dog.java
+++ src/Dog.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 
 public class Dog extends Actor {
   public Dog(Cell inLoc, boolean isHuman) {
-    super(inLoc, Color.YELLOW, isHuman, 1);
+    initActor(inLoc, Color.YELLOW, isHuman, 1);
   }
 
+  @Override
   protected void setPoly() {
     display = new ArrayList<Polygon>();
     Polygon ear1 = new Polygon();
-    ear1.addPoint(loc.x + 5, loc.y + 5);
-    ear1.addPoint(loc.x + 15, loc.y + 5);
-    ear1.addPoint(loc.x + 5, loc.y + 15);
+    ear1.addPoint(getLocation().x + 5, getLocation().y + 5);
+    ear1.addPoint(getLocation().x + 15, getLocation().y + 5);
+    ear1.addPoint(getLocation().x + 5, getLocation().y + 15);
     Polygon ear2 = new Polygon();
-    ear2.addPoint(loc.x + 20, loc.y + 5);
-    ear2.addPoint(loc.x + 30, loc.y + 5);
-    ear2.addPoint(loc.x + 30, loc.y + 15);
+    ear2.addPoint(getLocation().x + 20, getLocation().y + 5);
+    ear2.addPoint(getLocation().x + 30, getLocation().y + 5);
+    ear2.addPoint(getLocation().x + 30, getLocation().y + 15);
     Polygon face = new Polygon();
-    face.addPoint(loc.x + 8, loc.y + 7);
-    face.addPoint(loc.x + 27, loc.y + 7);
-    face.addPoint(loc.x + 27, loc.y + 25);
-    face.addPoint(loc.x + 8, loc.y + 25);
+    face.addPoint(getLocation().x + 8, getLocation().y + 7);
+    face.addPoint(getLocation().x + 27, getLocation().y + 7);
+    face.addPoint(getLocation().x + 27, getLocation().y + 25);
+    face.addPoint(getLocation().x + 8, getLocation().y + 25);
     display.add(face);
     display.add(ear1);
     display.add(ear2);
